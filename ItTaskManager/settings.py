0f9e3f967cbf781961ea3a471b68591c22458d60
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-01 13:16:26 +0200

    fix path in urls.py, add AUTH_USER_MODEL with name model 'task_manager.Worker', and change TIME_ZONE to Europe/Kiev

--- ItTaskManager/settings.py
+++ ItTaskManager/settings.py
@@ -107,12 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "task_manager.Worker"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
