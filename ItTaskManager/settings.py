ca76fa0101d81cc02f9e82efdf3c3a0719e16f17
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-01 16:54:58 +0200

    url.py add authentication path, settings.py configure LOGIN_REDIRECT_URL

--- ItTaskManager/settings.py
+++ ItTaskManager/settings.py
@@ -114,6 +114,7 @@ USE_I18N = True
 USE_TZ = True
 
 AUTH_USER_MODEL = "task_manager.Worker"
+LOGIN_REDIRECT_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
